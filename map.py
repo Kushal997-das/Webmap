2a2a9605e35cc5dd801108d4e06ce8a62cfd0f8d
kushal <daskushal980@gmail.com>
2020-12-12 23:02:34 +0530

    added names

--- map.py
+++ map.py
@@ -1,19 +1,10 @@
 import folium
-#from PIL import Image
-
-#ladakh = Image.open("ladakh.jpg")
-#taj_mahel=Image.open('taj mahel.jpg')
-#rammandir=Image.open('rammandir.jpg')
-
-
-
-
 m = folium.Map(tiles='Stamen Toner', zoom_start=4)
-iconSpiderman = folium.features.CustomIcon("ladakh.jpg", icon_size=(100,100))
-iconHulk = folium.features.CustomIcon("taj mahel.jpg", icon_size=(100,100))
-iconWolverine = folium.features.CustomIcon("rammandir.jpg", icon_size=(100,100))
-folium.Marker([40.743720, -73.822030], tooltip="Ladakh", popup='Ladakh,India', icon=iconSpiderman).add_to(m)
-folium.Marker([39.760979, -84.192200], tooltip="Taj Mahal", popup='Taj Mahal,India', icon=iconHulk).add_to(m)
-folium.Marker([54.464180, -110.182259], tooltip="Ayodhya RamMandir", popup='Ayodhya RamMandir,India', icon=iconWolverine).add_to(m)
+ladakh = folium.features.CustomIcon("ladakh.jpg", icon_size=(100,100))
+taj_mahhel = folium.features.CustomIcon("taj mahel.jpg", icon_size=(100,100))
+rammandir= folium.features.CustomIcon("rammandir.jpg", icon_size=(100, 100))
+folium.Marker([40.743720, -73.822030], tooltip="Ladakh", popup='Ladakh,India', icon=ladakh).add_to(m)
+folium.Marker([39.760979, -84.192200], tooltip="Taj Mahal", popup='Taj Mahal,India', icon=taj_mahhel).add_to(m)
+folium.Marker([54.464180, -110.182259], tooltip="Ayodhya RamMandir", popup='Ayodhya RamMandir,India', icon=rammandir).add_to(m)
 print(m)
 m.save('map.html')
